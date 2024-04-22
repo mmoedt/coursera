a4fd1f717d7ceed2c4902b5dddfb08bb6e3c468a
Michael Moedt <michael.moedt@treez.io>
2024-04-22 16:21:55 -0700

    Adding progress on initial project setup from scratch

---
+++ programming-with-javascript/setup-hello/src/index.js
@@ -0,0 +1,11 @@
+
+function main(args) {
+    console.log('Hello, World\x21');
+    return 0;
+}
+
+// TO-DO: `if __name__ == '__main__':`  style for javascript?
+if (!globalThis.under_test) {
+    main();
+}
+
---
+++ programming-with-javascript/setup-hello/src/tests.js
@@ -0,0 +1,8 @@
+
+// TO-DO: Import some helpful modules here
+// TO-DO: Import main code from index.js, and do some unit testing?
+
+globalThis.under_test = true;
+
+console.log('Running tests...');
+console.log('Done!');
