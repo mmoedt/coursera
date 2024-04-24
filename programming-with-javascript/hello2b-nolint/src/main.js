7bb5ae936bedbde03fcfa22dd139de55d195e6c2
Michael Moedt <michael.moedt@treez.io>
2024-04-24 08:25:09 -0700

    Adding another 'hello' skeleton, with nvm,npm,prettier but without huge eslint

---
+++ programming-with-javascript/hello2b-nolint/src/main.js
@@ -0,0 +1,11 @@
+// Start of main.js for project 'hello2'
+
+function main(args) {
+    console.log('Hello, World\x21');
+    return 0;
+}
+
+// TO-DO: `if __name__ == '__main__':`  style for javascript?
+if (!globalThis.under_test) {
+    main();
+}
