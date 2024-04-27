46996e55aac4749850a2f212920d35dffc714f65
Michael Moedt <michael.moedt@treez.io>
2024-04-27 11:53:13 -0700

    Adding mini project to play with JS module scope

---
+++ programming-with-javascript/learn/modscope/src/main.mjs
@@ -0,0 +1,7 @@
+import { mod1_hi as say_hi } from './mod1.mjs';
+
+const message = 'Hello, World!';
+console.log(message);
+
+say_hi();
+
---
+++ programming-with-javascript/learn/modscope/src/mod1.mjs
@@ -0,0 +1,8 @@
+import { log } from './helpers.mjs';
+
+var var1 = 'v1';
+let var2 = 'v2';
+
+export function mod1_hi() {
+    log('mod1 says \'hi\'');
+}
