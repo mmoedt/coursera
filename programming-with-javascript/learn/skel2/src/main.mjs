6bf0c3069a96ff8fdadff873e69842d732069903
Michael Moedt <michael.moedt@treez.io>
2024-05-10 13:07:10 -0700

    Adding lighter project skeleton

---
+++ programming-with-javascript/learn/skel2/src/main.mjs
@@ -0,0 +1,19 @@
+import { log } from './helpers.mjs';
+
+const g = globalThis;
+
+const emptyStr = '';
+const zero = 0;
+const falseVal = false;
+const nullVal = null;
+const undefVal = undefined;
+
+function show(expr) {
+  log(`The value of '${expr}' is '${JSON.stringify(eval(expr))}'.`);
+}
+
+show('zero');
+show('undefVal');
+show('zero || 1');
+show('zero ?? 1');
+// EOF
